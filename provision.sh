e799b12c55fa71f8d58ae5526df38da5366909b5
kenjis <kenji.uui@gmail.com>
2015-11-27 21:46:08 +0900

    Fix indent

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 # Modified 2015 by kenjis <https://github.com/kenjis/vagrant-centos7-php>
 #
 # Original
-#   @link https://github.com/progre/template-vagrant/blob/master/provision.sh
+#   @link    https://github.com/progre/template-vagrant/blob/master/provision.sh
 #   @license MIT, Copyright (c) 2015 PROGRE <https://github.com/progre/template-vagrant/blob/master/LICENSE>
 
 if ! [ `which ansible` ]; then
