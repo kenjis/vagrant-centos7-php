80445a68c19e81d4d6ab7aed9c76fbcd6375b1ba
kenjis <kenji.uui@gmail.com>
2015-12-01 13:25:11 +0900

    Workround to wait for mounting /mnt/project.

--- provisioning/roles/php-project/templates/remount-mnt-project.sh
+++ provisioning/roles/php-project/templates/remount-mnt-project.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# workround to wait for mounting /mnt/project
+while [ `df | grep /mnt/project | wc -l` == 0 ]
+do
+  sleep 5
+done
+
 # remount /mnt/project for changing gid
 mount | grep -q /mnt/project
 if [ $? -eq "0" ]; then
